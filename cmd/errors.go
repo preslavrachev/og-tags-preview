ad2c4676921b6278015ac709450c1bed644afa20
Preslav Rachev <rachev.preslav@gmail.com>
2025-06-24 11:18:24 +0200

    Fix typos

--- cmd/errors.go
+++ cmd/errors.go
@@ -19,37 +19,37 @@ func (app *application) errorResponse(w http.ResponseWriter, r *http.Request, st
 	}
 }
 
-// 400 bad request
+// 400 Bad Request
 func (app *application) badRequestResponse(w http.ResponseWriter, r *http.Request, err error) {
 	app.logError(r, err)
 	message := "received bad request"
 	app.errorResponse(w, r, http.StatusBadRequest, message)
 }
 
-// 404 not found
+// 405 Method Not Allowed
 func (app *application) resourceNotFoundResponse(w http.ResponseWriter, r *http.Request) {
 	message := "the requested resource could not be found"
 	app.errorResponse(w, r, http.StatusNotFound, message)
 }
 
-// 405 method not allow
+// 405 Method Not Allowed
 func (app *application) methodNotAllowedResponse(w http.ResponseWriter, r *http.Request) {
 	message := fmt.Sprintf("the %s method is not supported for this resource", r.Method)
 	app.errorResponse(w, r, http.StatusMethodNotAllowed, message)
 }
 
-// 422 unprocessable
+// 422 Unprocessable Content
 func (app *application) failedValidationResponse(w http.ResponseWriter, r *http.Request, err error) {
 	app.errorResponse(w, r, http.StatusUnprocessableEntity, err.Error())
 }
 
-// 429 too many request
+// 429 Too Many Requests
 func (app *application) rateLimitExceededResponse(w http.ResponseWriter, r *http.Request) {
 	message := "rate limit exceeded"
 	app.errorResponse(w, r, http.StatusTooManyRequests, message)
 }
 
-// 500
+// 500 Internal Server Error
 func (app *application) serverErrorResponse(w http.ResponseWriter, r *http.Request, err error) {
 	app.logError(r, err)
 	message := "the server encountered a problem and could not process your request"
